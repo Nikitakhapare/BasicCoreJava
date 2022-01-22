8b2c3505f3583878300050f96902be764129e99d
NikitaKhapare <NikitaKhapare53@gmail.com>
2022-01-21 21:24:59 -0500

    Calculating percentage of Haid vs Tail

---
+++ CoinFlip.java
@@ -0,0 +1,39 @@
+package com.BasicCoreProgram;
+
+import java.util.Scanner;
+
+public class CoinFlip {
+
+	public static void main(String[] args) {
+		
+	 Scanner sc = new Scanner(System.in);
+	double n,i, counter1=0,counter2=0;
+	
+	System.out.println("Enter a Number of time coin flip");
+	n =sc.nextDouble();
+	
+	for(i=1;i<=n;i++)
+	{
+	//getting number between 0 to 1
+    double rand= Math.random();
+	if(rand<0.5)
+	{
+		System.out.println("Its Tail ");
+	    counter1++;
+	}
+	else
+	{
+		System.out.println("Its Head");
+		counter2++;
+	}
+	}
+	System.out.println("No of time tails appear "+counter1);
+	System.out.println("No of time Head appear "+counter2);
+	
+	double percentage= (double)((counter2/n)*100);
+	System.out.println("persentange of haid  is "+percentage);
+	
+	System.out.println("persentange of tail is "+(100-percentage));
+}
+	
+}
