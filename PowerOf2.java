729c16eb1efe8e6326cdb38e2d829a71e3d1a86f
NikitaKhapare <NikitaKhapare53@gmail.com>
2022-01-21 22:06:52 -0500

    Calculating Power of 2

---
+++ PowerOf2.java
@@ -0,0 +1,21 @@
+package com.BasicCoreProgram;
+
+import java.util.Scanner;
+
+public class PowerOf2 {
+	public static void main(String[] args) {
+		int n,i,  result = 0; 
+		System.out.println("Enter a value of n");  
+		Scanner sc = new Scanner(System.in);
+		n=sc.nextInt();
+		for(i=1;i<=n;i+=31) 
+		{	
+			if(n<31)
+	           result = (int) Math.pow(2,n);
+			else
+				System.out.println("Invalid Input");
+		}
+	   System.out.println("Power "+result);
+	}
+
+}
