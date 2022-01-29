968179a41b053376a114bf635634963c6fbe8157
Nikita <nikitakhapare53@gmail.com>
2022-01-29 21:28:10 +0530

    Calculated Quotient Remainder

---
+++ QuotientRemainder.java
@@ -0,0 +1,21 @@
+package com.BasicCoreProgram;
+
+import java.util.Scanner;
+
+public class QuotientRemainder {
+
+	public static void main(String[] args) {
+	 Scanner sc=new Scanner(System.in);
+	 System.out.println("Enter a Number= ");
+     int  N=sc.nextInt();
+     System.out.println("Enter a divisor= ");
+     int i=sc.nextInt();
+     System.out.println("Quotient of Number "+N/i);
+     System.out.println("Reminder of Number "+N%i);
+
+	}
+
+	
+	}
+
+
