b7bb0a6a316b5bc78c292e889c0143dcca20b33a
Nikita <nikitakhapare53@gmail.com>
2022-02-01 19:48:10 +0530

    Updated Intendation

--- CoinFlip.java
+++ CoinFlip.java
@@ -4,28 +4,30 @@ import java.util.Scanner;
 
 public class CoinFlip {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+ {
 		
-	 Scanner sc = new Scanner(System.in);
-	double n,i, counter1=0,counter2=0;
+	double n,i, counter1=0,counter2=0; 
+        Scanner sc = new Scanner(System.in);
+	 
 	
-	System.out.println("Enter a Number of time coin flip");
+        System.out.println("Enter a Number of time coin flip");
 	n =sc.nextDouble();
 	
 	for(i=1;i<=n;i++)
 	{
 	//getting number between 0 to 1
-    double rand= Math.random();
-	if(rand<0.5)
-	{
-		System.out.println("Its Tail ");
-	    counter1++;
-	}
-	else
-	{
+             double rand= Math.random();
+	     if(rand<0.5)
+		{
+		  System.out.println("Its Tail ");
+	          counter1++;
+		}
+	    else
+		{
 		System.out.println("Its Head");
 		counter2++;
-	}
+		}
 	}
 	System.out.println("No of time tails appear "+counter1);
 	System.out.println("No of time Head appear "+counter2);
@@ -34,6 +36,6 @@ public class CoinFlip {
 	System.out.println("persentange of haid  is "+percentage);
 	
 	System.out.println("persentange of tail is "+(100-percentage));
-}
+ }
 	
 }
